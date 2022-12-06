9c380dddfcf310c4eeddb2467ec212598dd97944
slava_110 <slava.hero@bk.ru>
2022-12-06 09:55:58 +0300

    Fix do-while

--- labs/lab1/src/Lab_1_1.java
+++ labs/lab1/src/Lab_1_1.java
@@ -2,11 +2,19 @@ import java.util.stream.IntStream;
 
 public class Lab_1_1 {
 
+    /**
+     * Tips:
+     * ++i сначала увеличит значение i, а потом вернёт его
+     * i++ наборот: сначала возвращает, а потом увеличивает
+     *
+     * @param args аргументы программы
+     */
     public static void main(String[] args) {
         int[] array = new int[] {1, 10, 50, 100};
 
         System.out.println(arraySumFor(array));
         System.out.println(arraySumWhile(array));
+        System.out.println(arraySumDoWhile(array));
         System.out.println(arraySumForEach(array));
     }
 
@@ -31,6 +39,18 @@ public class Lab_1_1 {
             res += array[i];
             i++;
         }
+        return res;
+    }
+
+    /**
+     * Сумма чисел массива циклом while
+     */
+    public static int arraySumDoWhile(int[] array) {
+        int res = 0;
+        int i = 0;
+        do {
+            res += array[i];
+        } while(++i < array.length);
         return res;
     }
 
